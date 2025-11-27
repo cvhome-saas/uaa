1b980befafdc8583591a6ce4950edf00203b423c
Ashraf Mahmoud <ashraf1abdlrasool@gmail.com>
2025-11-27 21:26:14 +0300

    add login inside public module

--- src/main/resources/uaa-fe/src/app/pages/home/home.component.ts
+++ src/main/resources/uaa-fe/src/app/pages/home/home.component.ts
@@ -1,17 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {ShapeComponent} from '../public/componants/shape/shape.component';
 
 @Component({
   selector: 'app-home',
+  template: `
+    <router-outlet/>
+  `,
   imports: [
     RouterOutlet,
-    ShapeComponent
-  ],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  ]
 })
 export class HomeComponent {
-  title: string = 'Make cool store easily with Cvhome';
-  desc: string = 'Join our clients and try create your store with few clicks for free.';
 }
